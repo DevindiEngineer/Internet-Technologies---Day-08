8142de74157d24a5f512d3670a816d66545b3178
DevindiEngineer <devindicampus@gmail.com>
2026-07-25 09:18:25 +0530

    arrays string converts and back (json)

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 console.log("Hi!")
 
+/*
+//CRUD- create /read/update/delete
+
 //clear
 localStorage.clear()
 
-//insert data
+//create data
 localStorage.setItem("name","Wicky")
 localStorage.setItem("age","25")
 localStorage.setItem("country","Australia")
@@ -15,5 +18,32 @@ localStorage.setItem("name","dew")
 //if you want another name make a  new variable
 localStorage.setItem("name1","Pealy")
 
-//Delete
+//Delete/remove
 localStorage.removeItem("name1")
+
+let name = localStorage.getItem("name")
+console.log(name);*/
+
+localStorage.clear()
+
+let customer ={
+    name:"Kamal",
+    age:16,
+    isActive:true
+}
+
+//Object Object
+localStorage.setItem("customer",customer)
+console.log(localStorage.getItem("customer"))
+
+
+//converts to string and reverse back 
+let stringCustomer = JSON.stringify(customer)
+
+localStorage.setItem("customer",stringCustomer)
+
+let retCustomer = localStorage.getItem("customer")
+
+let jsonCustomer = JSON.parse(retCustomer)
+
+console.log(jsonCustomer)
