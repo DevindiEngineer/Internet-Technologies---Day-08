67355b8552639cb94bb7785e4873145d2325913f
DevindiEngineer <devindicampus@gmail.com>
2026-07-25 08:57:22 +0530

    Local storage intro

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("Hi!");
+
+//clear
+localStorage.clear()
+
+//insert data
+localStorage.setItem("name","Wicky")
+localStorage.setItem("age","25")
+localStorage.setItem("country","Australia")
+
+//UPDATE
+//update the name assign to same evariable
+localStorage.setItem("name","dew")
+
+//if you want another name make a  new variable
+localStorage.setItem("name1","Pealy")
